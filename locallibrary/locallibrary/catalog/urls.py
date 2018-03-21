6277c2a0264a8639274be9c41eaa413455b15033
Unknown <claramm@usal.es>
2018-03-21 19:06:53 +0100

    t

--- locallibrary/locallibrary/catalog/urls.py
+++ locallibrary/locallibrary/catalog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('mybooks/', views.LoanedBooksLibrariansListView.as_view(), name='borrowed'),
 ]
